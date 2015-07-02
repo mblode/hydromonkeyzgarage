459adf5fd0c14caef8219e5ae60fcdcbaf738a1f
mblode <m.blode@gmail.com>
2015-07-02 10:11:32 +1000

    Add files

--- Gemfile
+++ Gemfile
@@ -1,14 +1,25 @@
-# If you do not have OpenSSL installed, update
-# the following line to use "http://" instead
-source 'https://rubygems.org'
+# If you have OpenSSL installed, we recommend updating
+# the following line to use 'https'
+source 'http://rubygems.org'
 
-gem "middleman", "~>3.3.12"
+gem 'middleman', '~> 3.3.12'
+gem 'middleman-blog', '~> 3.5.3'
+gem 'middleman-deploy'
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+# For feed.xml.builder
+gem 'builder'
 
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'middleman-livereload'
+gem 'middleman-autoprefixer'
+gem "middleman-minify-html"
+gem 'middleman-imageoptim'
 
-# Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
+gem 'font-awesome-middleman'
+gem 'middleman-bootstrap-navbar'
+gem "bootstrap-sass", :require => false
+gem "jquery-middleman"
+
+gem 'therubyracer' # faster JS compiles
+gem 'oj' # faster JS compiles
+
+gem "nokogiri"
--- config.rb
+++ config.rb
@@ -1,4 +1,50 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+set :fonts_dir,  "source/fonts"
+
+activate :bootstrap_navbar
+
+sprockets.append_path File.join root, 'source/javascripts'
+
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions']
+end
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "portfolio"
+
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
+  blog.layout = "post"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+
+  blog.name = "portfolio"
+  blog.prefix = "portfolio"
+end
+
+
+page "/feed.xml", layout: false
+
+###
 # Compass
 ###
 
@@ -14,38 +60,38 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+activate :livereload
+
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def nav_active(path)
+    current_page.path == path ? {:class => "active"} : {}
+  end
+
+end
 
 set :css_dir, 'stylesheets'
 
@@ -56,17 +102,31 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  activate :minify_html
+
+  activate :imageoptim
+
+  activate :gzip
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+  deploy.build_before = true
+end
+
+activate :directory_indexes
