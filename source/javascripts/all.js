a9c95ccbc32bd4666d60aa91ac3037dfda602bd8
mblode <mblode@Matthews-iMac.local>
2015-07-25 15:55:06 +1000

    A WHOLE BUNCH. Like a lot of work.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,29 @@
 //= require jquery
 //= require bootstrap-sprockets
+//= require '_flickity.js'
 
 'use strict';
 
 $(document).ready(function() {
+
+  $('.gallery').flickity({
+    accessibility: true,
+    autoPlay: 3000,
+    cellAlign: 'center',
+    cellSelector: undefined,
+    contain: false,
+    draggable: true,
+    freeScroll: false,
+    friction: 0.2,
+    initialIndex: 0,
+    imagesLoaded: true,
+    percentPosition: true,
+    prevNextButtons: true,
+    pageDots: true,
+    resize: true,
+    rightToLeft: false,
+    setGallerySize: true,
+    watchCSS: false,
+    wrapAround: true
+  });
 });
