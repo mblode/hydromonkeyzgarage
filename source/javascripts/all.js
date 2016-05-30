32dfff3b406b4540b243a9979e31dbc6a7cf3c82
Matthew Blode <m.blode@gmail.com>
2016-05-30 19:58:00 +1000

    Add

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,21 +7,7 @@
 $(document).ready(function() {
 
   $('.gallery').flickity({
-    accessibility: true,
-    autoPlay: true,
-    cellAlign: 'center',
-    cellSelector: undefined,
-    contain: false,
-    draggable: true,
-    freeScroll: false,
-    friction: 0.2,
-    initialIndex: 0,
     imagesLoaded: true,
-    prevNextButtons: true,
-    pageDots: true,
-    resize: true,
-    setGallerySize: true,
-    watchCSS: false,
     wrapAround: true
   });
 
